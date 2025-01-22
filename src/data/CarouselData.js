75fd8bf6e10b0231a4424861b356e27b7714c52d
Tere Monardes <teremonardes.s@gmail.com>
2025-01-22 18:45:17 -0300

    baseHome

---
+++ src/data/CarouselData.js
@@ -0,0 +1,24 @@
+import ceramica from '../assets/images/ceramica.jpg'
+
+const carouselData = [
+    {
+      src: ceramica,
+      alt: "Ceramica",
+      label: "LOZAS",
+      description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
+    },
+    {
+      src: "https://via.placeholder.com/800x400?text=Slide+2",
+      alt: "Second slide",
+      label: "VIDRIOS",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    },
+    {
+      src: "https://via.placeholder.com/800x400?text=Slide+3",
+      alt: "Third slide",
+      label: "METALES",
+      description: "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+    },
+  ];
+  
+  export default carouselData;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,46 @@
+import CarouselF from "../../components/Carousel/Carousel"
+import Card from 'react-bootstrap/Card'
+import './Home.css'
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <CarouselF></CarouselF>
+    <div className="homeMission">
+      <div className="homeTitle">NUESTRA MISIÓN</div>
+      <div className="homeText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dicta pariatur nemo? Exercitationem possimus tempora laudantium quisquam incidunt quaerat eos voluptatem repellat cum eius voluptatibus praesentium, nemo rem aperiam quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, mollitia iusto vero voluptates veritatis voluptatibus? Quis explicabo ipsam quisquam aperiam. Quis distinctio non minima voluptas neque illo provident obcaecati consequatur.</div>
+    </div>
+
+    <div className="catalogoSection">
+<div className="titleSection">MATERIALIDAD</div>
+<div className="cardSection">
+  <div className="vidriosCard">
+<Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>VIDRIOS</Card.Title>
+      </Card.Body>
+    </Card>
+    </div>
+    <div className="metalesCard">
+<Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>METALES</Card.Title>
+      </Card.Body>
+    </Card>
+    </div>
+    <div className="lozasCard">
+<Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>LOZAS</Card.Title>
+      </Card.Body>
+    </Card>
+    </div>
+</div>
+    </div>
+    </>
   )
 }
 
